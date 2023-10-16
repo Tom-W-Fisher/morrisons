28d74266766bec5d0c070b2837710874eb7249c3
narrowai <dillonkdd@gmail.com>
2023-10-16 23:16:26 +0100

    temp display

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('wow')
+class board:
+	def __init__(self):
+		# three rings, 8 places each
+		self.places = [[' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ']]
+	def display(self):
+		text = ''
+		text += f'{self.places[0][0]} - - {self.places[0][1]} - - {self.places[0][2]}\n'
+		text += f'| {self.places[1][0]} - {self.places[1][1]} - {self.places[1][2]} |\n'
+		text += f'| | {self.places[2][0]} {self.places[2][1]} {self.places[2][2]} | |\n'
+		# needs like 5 more lines
+		return text
+
+
+class piece:
+	def __init__(self):
+		pass
+
+b = board()
+print(b.display())
