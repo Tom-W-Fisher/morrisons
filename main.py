385e88b5049ffa703b8d70654c492e8df5972d7b
Tom-W-Fisher <up2207168@myport.ac.uk>
2023-10-16 23:58:23 +0100

    Did nothing pretty much

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-class board:
+class Board:
 	def __init__(self):
 		# three rings, 8 places each
-		self.places = [[' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ']]
+		self.places = [	[0,0,0,0,0,0,0,0],
+				 		[0,0,0,0,0,0,0,0],
+						[0,0,0,0,0,0,0,0]]
+		# I prefer using ints here, because they are slightly smaller (28 vs 49 bytes)
+		# Also prefer to align 2d arrays like that during declaration (unless declaring using inline for loop syntax)
+		# i.e.
+		# self.places = [[0 for _ in range(8)] for _ in range(3)]
+
+		# I'll going to be bad and say
+
 	def display(self):
-		text = ''
-		text += f'{self.places[0][0]} - - {self.places[0][1]} - - {self.places[0][2]}\n'
-		text += f'| {self.places[1][0]} - {self.places[1][1]} - {self.places[1][2]} |\n'
-		text += f'| | {self.places[2][0]} {self.places[2][1]} {self.places[2][2]} | |\n'
+		text = (
+		f"{self.places[0][0]} - - - {self.places[0][1]} - - - {self.places[0][2]}\n"
+		f"| {self.places[1][0]} - - {self.places[1][1]} - - {self.places[1][2]} |\n"
+		f"| | {self.places[2][0]} - {self.places[2][1]} - {self.places[2][2]} | |\n"
+		)
 		# needs like 5 more lines
 		return text
 
@@ -15,5 +25,5 @@ class piece:
 	def __init__(self):
 		pass
 
-b = board()
+b = Board()
 print(b.display())
