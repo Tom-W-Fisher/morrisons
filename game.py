b67d98cb7693edd0dd1064d4ec6a2582bd786dcb
Tom-W-Fisher <up2207168@myport.ac.uk>
2023-10-18 21:47:36 +0100

    Added function to convert player input to a move

--- game.py
+++ game.py
@@ -30,7 +30,42 @@ class Board:
 			self.places[1][6], self.places[1][5], self.places[1][4],
 			self.places[0][6], self.places[0][5], self.places[0][4])
 	
-	def add(self, ring: int, notch: int, player: int) -> None: # changed from "pieces" to disambiguate
+	def turn(self, move: Move):
+		"""Handles the logic for 1 turn. Does not check whos turn it is"""
+		if move.is_valid():
+			move.apply(self.places)
+			if self.mill_formed(move.new_ring, move.new_notch):
+				self.do_mill_stuff()
+
+	def mill_formed(self, ring: int, notch: int) -> bool: # changed to "mill_formed" for conciseness, can change back if preferred
+		# if notch even
+		if notch % 2 == 0:
+			# check along both edges of that ring if there's three
+			# i split the if statement over 2 lines
+			if self.places[ring][(notch-1)%8] == self.places[ring][notch] \
+		   and self.places[ring][(notch+1)%8] == self.places[ring][notch]:
+				return True
+		# else notch odd
+		else:
+			# check along edge of ring
+			if self.places[ring][(notch-1)%8] == self.places[ring][notch] \
+		   and self.places[ring][(notch+1)%8] == self.places[ring][notch]:
+				return True
+			
+			# check along lines that connect rings
+			if self.places[(ring+1)%3][(notch)] == self.places[ring][notch] \
+		   and self.places[(ring-1)%3][(notch)] == self.places[ring][notch]:
+				return True
+
+		# No conditions were met -> return False
+		return False
+
+	def do_mill_stuff():
+		"""Does mill stuff"""
+		pass
+
+	'''		
+	def add(self, ring: int, notch: int, player: int) -> None: 
 		"""Updates the value of places[ring][notch] to the value `player`"""
 		self.places[ring][notch] = player
 	
@@ -61,42 +96,20 @@ Returns a True/False depending on whether the move was successful"""
 			if new_ring != old_ring+1 and new_ring != old_ring-1:
 				return False
 		return True
-	
-	def mill_formed(self, ring: int, notch: int) -> bool: # changed to "mill_formed" for conciseness, can change back if preferred
-		# if notch even
-		if notch % 2 == 0:
-			# check along both edges of that ring if there's three
-			# i split the if statement over 2 lines
-			if self.places[ring][(notch-1)%8] == self.places[ring][notch] \
-		   and self.places[ring][(notch+1)%8] == self.places[ring][notch]:
-				return True
-		# elif notch odd
-		elif notch%2 == 1:
-			# check along edge of ring but also the line that cuts thru rings
-			pass
-
-	def turn(self, move: Move):
-		"""Handles the logic for 1 turn. Does not check whos turn it is"""
-		if move.is_valid():
-			move.apply(self.places)
-			if self.mill_formed(move.new_ring, move.new_notch):
-				self.do_mill_stuff()
-
-	def do_mill_stuff():
-		"""Does mill stuff"""
-		pass
-	
+	'''
 
 # I'm adding a bunch of OOP stuff because hehe
 class Player:
 	def __init__(self, token: int):
 		self.token = token
+		# "token" just refers to the arbitrary number for the player
 		self.men_unplayed = 9
 		self.men_in_play = 0
-		self.locations = []
+		self.men_locations = []
 
 class Move:
-	"""Superclass for the particular move classes"""
+	"""Superclass for the particular move classes.\
+ Not intended for direct use"""
 	def __init__(self, player: Player, new_ring, new_notch):
 		self.player = player
 		self.new_ring = new_ring
@@ -116,6 +129,7 @@ current board state."""
 	
 	def apply(self, places: list[list[int]]):
 		places[self.new_ring][self.new_notch] = self.player.token
+		self.player.men_locations.append([self.new_ring, self.new_notch])
 
 class Place(Move):
 	"""Describes a move type where a piece is placed on to the board"""
@@ -152,9 +166,48 @@ class Relocate(Move):
 		self.old_ring = old_ring
 		self.old_notch = old_notch
 
+def convert_to_move(text_input: str, player: Player) -> Move:
+	"""Parses a text (string) input into a move."""
 
+	text_input = text_input.split()
+	if len(text_input) > 2:
+		raise ValueError(f"Expected maximum of two position arguments,\
+				    {len(text_input)} were given: {text_input}")
+	
+	for index, word in enumerate(text_input):
+		if len(word) not in (2, 3):
+			raise ValueError(f"Expected each argument to be 2 or 3 characters \
+long: {word} was given at index {index}, which is of length {len(word)}")
+		
+		if not word[0].isalpha():
+			raise ValueError("Expected first character to be a letter. \
+{word[0]} was recieved.")
+		
+		word = word.upper().split(",")
+		text_input[index] = word
+
+		if word[0] not in ("A", "B", "C"):
+			raise ValueError("Expected first character to be either \
+A, B, or C. {word[0]} was recieved")
+
+		if not word[1].isdigit():
+			raise ValueError("Expected second character to be a digit.\
+{word[1]} was recieved.")
+		if word[1] < 0 or word[1] > 7:
+			raise ValueError(f"Expected second digit to be in range 0:7 \
+inclusive. Value of {word[0]} was recieved.")
+
+	if len(text_input) == 1:
+		return Place(player, ord(text_input[0][0]) -65, text_input[0][1])
+	
+	elif player.men_in_play > 3:
+		return Shift(player, ord(text_input[1][0]) -65, text_input[1][1],
+			   				 ord(text_input[0][0]) -65, text_input[0][1])
+	else:
+		return Relocate(player, ord(text_input[1][0]) -65, text_input[1][1],
+			   					ord(text_input[0][0]) -65, text_input[0][1])
 
 
 
 b = Board()
-print(b.display())
+print(str(b))
