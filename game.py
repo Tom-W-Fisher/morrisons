1da73efaabc743ff005c76c4d9295f94f497794e
Tom-W-Fisher <up2207168@myport.ac.uk>
2023-10-20 23:08:53 +0100

    Finished (?) and made it easier to read (?)

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
+
+# ----------------------------------------------------------------------------
+
 class Board:
 	"""Stores the state of the board and has a few methods for interacting \
 with it. \n
@@ -22,6 +25,8 @@ Attributes:
 
 		self.players = [Player(player_1_token), Player(player_2_token)]
 		self._current_player: int = 0
+
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 		
 	def get_current_player(self) -> int:
 		"""Returns the token of the current player"""
@@ -39,33 +44,7 @@ Attributes:
 		"""Changes to current player to the other player"""
 		self._current_player = (self._current_player + 1) % 2
 
-	def __str__(self):
-		return """\
-{} - - {} - - {}
-| {} - {} - {} |
-| | {} {} {} | |
-{} {} {}   {} {} {}
-| | {} {} {} | |
-| {} - {} - {} |
-{} - - {} - - {}
- """.format(self.places[0][0], self.places[0][1], self.places[0][2],
-			self.places[1][0], self.places[1][1], self.places[1][2],
-			self.places[2][0], self.places[2][1], self.places[2][2], 
-			self.places[0][7], self.places[1][7], self.places[2][7], self.places[0][3], self.places[1][3], self.places[2][3],
-			self.places[2][6], self.places[2][5], self.places[2][4], 
-			self.places[1][6], self.places[1][5], self.places[1][4],
-			self.places[0][6], self.places[0][5], self.places[0][4])
-	
-	def take_turn(self, player_input: str):
-		"""Handles the logic for 1 turn. Does not check whos turn it is."""
-		move = convert_to_move(player_input,
-						  self._get_current_player_object(),
-						  self)
-		move.apply(self.places)
-		if self.mill_formed(move.new_ring, move.new_notch):
-			move.player.mill_turn = True
-		else:
-			self.change_current_player()
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 	def mill_formed(self, ring: int, notch: int) -> bool:
 		"""Checks if the place specified by the given ring and notch is a \
@@ -89,12 +68,49 @@ Attributes:
 			elif self.places[(ring+1)%3][(notch)] == self.places[ring][notch] \
 		     and self.places[(ring-1)%3][(notch)] == self.places[ring][notch]:
 				return True
-
+			
 		# No conditions were met -> return False
 		return False
+	
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+	
+	def take_turn(self, player_input: str):
+		"""Handles the logic for 1 turn. Does not check whos turn it is.
+		Will raise errors (of type `ValueError`) with messages if input is not
+		formatted correctly or is not a legal move."""
+		move = convert_to_move(
+			player_input,
+			self._get_current_player_object(),
+			self)
+		move.apply(self.places)
+		if self.mill_formed(move.new_ring, move.new_notch):
+			move.player.mill_turn = True
+		else:
+			self.change_current_player()
+
+	
+	def __str__(self):
+		return """\
+{} - - {} - - {}
+| {} - {} - {} |
+| | {} {} {} | |
+{} {} {}   {} {} {}
+| | {} {} {} | |
+| {} - {} - {} |
+{} - - {} - - {}
+ """.format(self.places[0][0], self.places[0][1], self.places[0][2],
+			self.places[1][0], self.places[1][1], self.places[1][2],
+			self.places[2][0], self.places[2][1], self.places[2][2], 
+			self.places[0][7], self.places[1][7], self.places[2][7], 
+				self.places[0][3], self.places[1][3], self.places[2][3],
+			self.places[2][6], self.places[2][5], self.places[2][4], 
+			self.places[1][6], self.places[1][5], self.places[1][4],
+			self.places[0][6], self.places[0][5], self.places[0][4])
+	
+# ----------------------------------------------------------------------------
 
 class Player:
-	"""Stores some information about a player"""
+	"""Stores some information about a player."""
 	def __init__(self, token: int):
 		"""`token` is the number that will be \
 		used to represent the player's pieces when the board is converted to \
@@ -108,6 +124,8 @@ class Player:
 	@property
 	def men_in_play(self) -> int:
 		return len(self.men_locations)
+	
+# ----------------------------------------------------------------------------
 
 class Move:
 	"""Superclass for the particular move classes.\
@@ -129,9 +147,13 @@ current board state."""
 		places[self.new_ring][self.new_notch] = self.player.token
 		self.player.men_locations.append([self.new_ring, self.new_notch])
 
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
 class Remove(Move):
+	"""Describes a move where an enemy piece is removed from the board \
+		(the turn after a player forms a mill)"""
 	def __init__(self, player: Player, new_ring, new_notch, board: Board):
-		super().__init__()
+		super().__init__(player, new_ring, new_notch)
 		self.affected_player = board._get_other_player_object()
 		self.board = board
 
@@ -154,15 +176,20 @@ all enemy men are in mills.")
 		places[self.new_ring][self.new_notch] = 0
 		self.affected_player.men_locations.remove(
 											[self.new_ring, self.new_notch])
+		self.player.mill_turn = False
+		
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 class Place(Move):
 	"""Describes a move type where a piece is placed on to the board"""
 	def _check_validity(self, places: list[list[int]]):
-		super()._check_validity()
+		super()._check_validity(places)
 	
 	def apply(self, places):
 		super().apply(places)
 		self.player.men_unplayed -= 1
+
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 class Relocate(Move):
 	"""Describes a move type where a piece is moved from one space to any\
@@ -172,8 +199,11 @@ class Relocate(Move):
 		self.old_ring = old_ring
 		self.old_notch = old_notch
 
-	def _check_validity(self, places: list[list[int]], current_player: Player):
-		super()._check_validity()
+	def _check_validity(self, places: list[list[int]]):
+		super()._check_validity(places)
+		if places[self.old_ring][self.old_notch] != self.player.token:
+			raise ValueError("Initial place is not occupied by the moving \
+player's man.")
 		# `convert_to_move` only creates a `Relocate` object if the player has 3 men
 		# so checking number of men is not required
 		# which allows `Shift` to inherit without creating problems
@@ -185,20 +215,35 @@ class Relocate(Move):
 		self.player.men_locations.append(([self.new_ring][self.new_notch]))
 		self.player.men_locations.remove(([self.old_ring][self.old_notch]))
 
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
 class Shift(Relocate):
-	"""Describes a move where a piece is moved from one space to an \
-adjacent space"""
+	"""Describes a move where a piece is moved from one space to an adjacent \
+	space"""
 	def __init__(self, player, new_ring, new_notch, old_ring, old_notch):
-		super().__init__(self, player, new_ring, new_notch)
-		self.old_ring = old_ring
+		super().__init__(self, player, new_ring, new_notch, old_ring, old_notch)
 
-	def _check_validity(self, places: list[list[int]], current_player: Player):
-		super()._check_validity()
-
+	def _check_validity(self, places: list[list[int]]):
+		super()._check_validity(places)
+		
+		# big if clause
+		if self.new_ring == self.old_ring \
+			and (self.new_notch == (self.old_notch+1)%8 \
+			  or self.new_notch == (self.old_notch-1)%8) \
+			  \
+		or (self.new_notch == self.old_notch \
+		 	and self.new_notch % 2 == 1 \
+				and (self.new_ring == self.old_ring - 1 \
+			      or self.new_ring == self.old_ring + 1)):
+				pass
+		else:
+			raise ValueError("The initial position and destination position \
+are not adjacent.")
 
 	def apply(self, places):
 		return super().apply(places)
-
+	
+# ----------------------------------------------------------------------------
 
 def convert_to_move(text_input: str, player: Player, board: Board) -> Move:
 	"""Parses a text (string) input into a move."""
@@ -245,6 +290,7 @@ inclusive. Value of {word[0]} was recieved.")
 		return Relocate(player, ord(text_input[1][0]) -65, text_input[1][1],
 			   					ord(text_input[0][0]) -65, text_input[0][1])
 
+# ----------------------------------------------------------------------------
 
 
 b = Board(1, 2)
